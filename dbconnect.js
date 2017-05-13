db7982b6f8e4bbce6bf519dfdb046f8639d84c68
Jim Breslin <jbreslin33@gmail.com>
2017-05-13 19:11:09 -0400

    pg on same page as login

--- dbconnect.js
+++ dbconnect.js
@@ -8,11 +8,6 @@ var conString = "postgres://postgres:mibesfat@localhost/openrpg";
 var client = new pg.Client(conString);
 client.connect();
 
-// client.query("CREATE TABLE IF NOT EXISTS emps(firstname varchar(64), lastname varchar(64))");
-// client.query("INSERT INTO emps(firstname, lastname) values($1, $2)", ['Ronald', 'McDonald']);
-// client.query("INSERT INTO emps(firstname, lastname) values($1, $2)", ['Mayor', 'McCheese']);
-// client.query("INSERT INTO emps(firstname, lastname) values($1, $2)", ['Mayor', 'McCheese']);
-
 var query_string = "SELECT id, name FROM class ORDER BY name";
 
 
--- server.js
+++ server.js
@@ -41,5 +41,28 @@ function processAllFieldsOfTheForm(req, res) {
     });
 }
 
+
+//db
+var pg = require("pg");
+
+var conString = "postgres://postgres:mibesfat@localhost/openrpg";
+
+
+var client = new pg.Client(conString);
+client.connect();
+
+var query_string = "SELECT id, name FROM class ORDER BY name";
+
+
+var query = client.query(query_string);
+query.on("row", function (row, result) {
+    result.addRow(row);
+});
+query.on("end", function (result) {
+    console.log(JSON.stringify(result.rows, null, "    "));
+    client.end();
+});
+
+
 server.listen(3000);
 console.log("server listening on 3000");
