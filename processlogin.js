14469b6c5b83ece2402081628a19c07c1b2fc8b9
Jim Breslin <jbreslin33@gmail.com>
2017-05-17 19:52:39 -0400

    db call is in process login

--- processlogin.js
+++ processlogin.js
@@ -1,21 +1,43 @@
 var formidable = require("formidable");
+var util = require('util');
+
 
 var processLogin = function(req,res)
 {
-var form = new formidable.IncomingForm();
+	var form = new formidable.IncomingForm();
 
-form.on('field', function(field, value) {
-        if (field == 'username')
-        {
-                mUsername = value;
-                console.log('mUsername:' + mUsername);
-                dbcall();
-        }
-        if (field == 'password')
-        {
-                mPassword = value;
-                console.log('mPassword:' + mPassword);
-        }
+	form.on('field', function(field, value) 
+	{
+        	if (field == 'username')
+        	{
+                	mUsername = value;
+			var pg = require("pg");
+
+			var conString = "postgres://postgres:mibesfat@localhost/openrpg";
+
+			var client = new pg.Client(conString);
+			client.connect();
+
+			var query_string = "SELECT username, password FROM users where username = '" + mUsername + "';";
+			console.log('query_string:' + query_string);
+
+			var query = client.query(query_string);
+			query.on("row", function (row, result) 
+			{
+    				result.addRow(row);
+			});
+			query.on("end", function (result) 
+			{
+    				console.log(JSON.stringify(result.rows, null, "    "));
+    				client.end();
+			});
+
+        	}
+        	if (field == 'password')
+        	{
+                	mPassword = value;
+                	console.log('mPassword:' + mPassword);
+        	}
 });
 
     form.parse(req, function (err, fields, files) {
--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 var http = require('http');
-var util = require('util');
 
 var dl = require('./displaylogin');
 var pl = require('./processlogin');
@@ -17,29 +16,6 @@ app.get('/login', function (req, res) {
 app.post('/login', function (req, res) {
         pl(req,res);
 })
-
-//db
-function dbcall() {
-var pg = require("pg");
-
-var conString = "postgres://postgres:mibesfat@localhost/openrpg";
-
-var client = new pg.Client(conString);
-client.connect();
-
-var query_string = "SELECT username, password FROM users where username = '" + mUsername + "';"; 
-console.log('query_string:' + query_string);
-
-var query = client.query(query_string);
-query.on("row", function (row, result) {
-    result.addRow(row);
-});
-query.on("end", function (result) {
-    console.log(JSON.stringify(result.rows, null, "    "));
-    client.end();
-});
-
-}
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
