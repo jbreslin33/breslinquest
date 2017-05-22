f6fe01166692e954e4ccf425418dad13bf64638d
Jim Breslin <jbreslin33@gmail.com>
2017-05-21 22:40:22 -0400

    af

---
+++ db.js
@@ -0,0 +1,33 @@
+var pg = require('pg');
+var conString = "postgres://postgres:mibesfat@localhost/openrpg";
+
+var resp = [];
+
+function executeQuery (callback) {
+
+    pg.connect(conString, function(err, client, done) {
+        if(err) {
+            return console.error('error fetching client', err);
+        }
+
+	//var query_string = "SELECT username, password FROM users where username = '" + this.mClientUsername + "';";
+	var username = 'jbreslin';	
+	var query_string = "SELECT username, password FROM users where username = '" + username + "';";
+        client.query(query_string, function(err, result) {
+
+        done();
+
+        if(err) {
+            return console.error('error running query', err);
+        }
+
+        resp.push(result.rows[0]);
+        // console.log(res);
+
+        callback(resp);
+
+       });
+   });
+};
+
+exports.executeQuery = executeQuery;
