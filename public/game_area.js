9612b5c98315b11034289b815067f32cb9e5b993
Jim Breslin <jbreslin33@gmail.com>
2017-05-23 21:06:55 -0400

    mootools on client using game area which is html cacanvas

--- public/game_area.js
+++ public/game_area.js
@@ -1,16 +1,23 @@
-var myGameArea = {
-    canvas : document.createElement("canvas"),
-    start : function() {
-        this.canvas.width = 300;
+var GameArea = new Class(
+{
+        initialize: function()
+        {
+                console.log('test const');
+		this.canvas = document.createElement("canvas"); 
+        },
+
+	start: function()
+	{
+	this.canvas.width = 300;
         this.canvas.height = 300;
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
         this.frameNo = 0;
-        this.interval = setInterval(updateGameArea, 20);
+        this.interval = setInterval(updateGameArea, 20); 
         window.addEventListener('keypress', function (e) {
             e.preventDefault();
-            myGameArea.keys = (myGameArea.keys || []);
-            myGameArea.keys[e.keyCode] = (e.type == "keypress");
+            gameArea.keys = (gameArea.keys || []);
+            gameArea.keys[e.keyCode] = (e.type == "keypress");
 
         if (e.keyCode == 37)
         {
@@ -40,12 +47,15 @@ var myGameArea = {
 
         window.addEventListener('keyup', function (e) {
         })
-    },
-    stop : function() {
-        clearInterval(this.interval);
-    },
-    clear : function() {
-        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-    }
-}
 
+	},
+    
+	stop : function() 
+	{
+        	clearInterval(this.interval);
+    	},
+    	clear : function() 
+	{
+        	this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    	}
+});
