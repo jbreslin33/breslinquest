2cf2715cf08c9431ff7e14783942c25eb9e8fbae
Jim Breslin <jbreslin33@gmail.com>
2017-05-23 20:04:17 -0400

    got images

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 require ('mootools');
 
 //my exports
