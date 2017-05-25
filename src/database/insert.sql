44ac873a233bffe54e329b2d4c094ee3216fa247
Jim Breslin <jbreslin33@gmail.com>
2017-05-25 12:06:17 -0400

    adding party

--- src/database/insert.sql
+++ src/database/insert.sql
@@ -23,6 +23,8 @@ insert into class (name) values ('Illusionist');
 insert into characters (name,user_id,race_id,class_id,x,y,z,d,full_hitpoints,current_hitpoints,level,experience) values ('Julius',1,1,1,0,0,0,0,13,13,1,0);
 insert into characters (name,user_id,race_id,class_id,x,y,z,d,full_hitpoints,current_hitpoints,level,experience) values ('Ren Blackis',1,4,4,0,0,0,0,13,13,1,0);
 insert into characters (name,user_id,race_id,class_id,x,y,z,d,full_hitpoints,current_hitpoints,level,experience) values ('Phantazar',1,1,8,0,0,0,0,13,13,1,0);
+insert into characters (name,user_id,race_id,class_id,x,y,z,d,full_hitpoints,current_hitpoints,level,experience) values ('Midget Man',1,5,3,0,0,0,0,13,13,1,0);
+insert into characters (name,user_id,race_id,class_id,x,y,z,d,full_hitpoints,current_hitpoints,level,experience) values ('Barton',1,1,5,0,0,0,0,13,13,1,0);
 
 --monsters
 insert into characters (race_id,x,y,z,d,full_hitpoints,current_hitpoints,level,experience) values (2,2,0,0,0,3,3,1,0);
