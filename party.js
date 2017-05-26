860c058e585021b946aec73f8d1a363030909e4c
Jim Breslin <jbreslin33@gmail.com>
2017-05-26 11:57:10 -0400

    update db

--- party.js
+++ party.js
@@ -1,3 +1,5 @@
+var pg = require('pg');
+
 var Party = new Class(
 {
         initialize: function(bapp,id,name,d,x,y,z,userid)
@@ -23,8 +25,38 @@ var Party = new Class(
 		this.x = x;
 		this.y = y;
 		this.z = z;
+		this.dbUpdatePosition();
 		console.log('d:' + d + ' x:' + x + 'y:' + y + 'z:' + z);  
 	},
+
+        dbUpdatePosition: function()
+        {
+                var conString = "postgres://postgres:mibesfat@localhost/openrpg";
+		
+                var queryString = 'update parties set d = ' + this.d + ', x = ' + this.x + ', y = ' + this.y + ', z = ' + this.z + ';';
+//update parties set d = 1, x = 2, y = 3, z = 4 where user_id = 1 AND name = 'Classic';
+
+                console.log('update party db:' + queryString);
+
+                var client = new pg.Client(conString);
+                client.connect();
+
+                var query = client.query(queryString, function (err,result)
+                {
+                        if (err)
+                        {
+                                throw err;
+                        }
+                });
+                query.on("row", function (row,result)
+                {
+                        //result.addRow(row);
+                });
+                query.on("end", function (result)
+                {
+                        client.end();
+                }.bind(this));
+        },
 
 	getd: function()
 	{
