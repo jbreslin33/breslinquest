796565c7dcb80dd57bf0fb3b84864e395b79de2a
Jim Breslin <jbreslin33@gmail.com>
2017-06-11 17:49:29 -0400

    fixed negative bug

--- src/database/build_insert.sh
+++ src/database/build_insert.sh
@@ -1,4 +1,4 @@
 sudo -u postgres psql -d openrpg -f src/database/build.sql
 sudo -u postgres psql -d openrpg -f src/database/insert.sql
-sudo -u postgres psql -d openrpg -f src/database/make_world_points.sql
+./src/database/make_world_points.sh
 sudo node index.js
--- src/database/make_world_points.sh
+++ src/database/make_world_points.sh
@@ -8,11 +8,11 @@ rm src/database/make_world_points.sql
 
 ((i = 1))
 
-for (( x = 0; x <= 10; x++ ))      ### Outer x loop ###
+for (( x = -10; x <= 10; x++ ))      ### Outer x loop ###
 do
-    for (( y = 0 ; y <= 10; y++ )) ### Inner y  loop ###
+    for (( y = -10 ; y <= 10; y++ )) ### Inner y  loop ###
     do
-    	for (( z = 0 ; z <= 10; z++ )) ### Inner z  loop ###
+    	for (( z = -10 ; z <= 10; z++ )) ### Inner z  loop ###
 	do
 		echo "insert into world_points (x,y,z) values (" $x "," $y ","  $z ");" >> src/database/make_world_points.sql
 		echo "insert into world_directions (passable,picture_id,d,world_point_id) values (1,4,0,$i);" >> src/database/make_world_points.sql
