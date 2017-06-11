25368cd4cc34a60c7a0dd04318fa74c99fc85cb5
Jim Breslin <jbreslin33@gmail.com>
2017-06-11 13:19:40 -0400

    passable

--- public/dungeon_master_screen.js
+++ public/dungeon_master_screen.js
@@ -11,31 +11,57 @@ var DungeonMasterScreen = new Class(
     		this.button.appendChild(this.buttonText);
     		document.body.appendChild(this.button);        
 
-                this.mSelect = document.createElement("SELECT");
-                this.mSelect.setAttribute("id", "setSelect");
-                document.body.appendChild(this.mSelect);
+		//picture
+                this.mPictureSelect = document.createElement("SELECT");
+                this.mPictureSelect.setAttribute("id", "pictureSelect");
+                document.body.appendChild(this.mPictureSelect);
 
                 //fill picture select
-                for (i=0; i < pictureNameArray.length; i++)
+                for (var i=0; i < pictureNameArray.length; i++)
                 {
                 	var z = document.createElement("option");
                         z.setAttribute("value", "" + pictureIDArray[i]);
                         var t = document.createTextNode("" + pictureNameArray[i]);
                         z.appendChild(t);
-                        this.mSelect.appendChild(z);
+                        this.mPictureSelect.appendChild(z);
                 }
 		
-		var that = this;
+		//passable
+                this.mPassableSelect = document.createElement("SELECT");
+                this.mPassableSelect.setAttribute("id", "passableSelect");
+                document.body.appendChild(this.mPassableSelect);
+
+                //fill passable select
+		var passableIDArray = new Array();
+		passableIDArray.push(1);	
+		passableIDArray.push(2);	
 		
+		var passableNameArray = new Array();
+		passableNameArray.push("passable");	
+		passableNameArray.push("not passable");	
+
+                for (var i=0; i < passableNameArray.length; i++)
+                {
+                        var z = document.createElement("option");
+                        z.setAttribute("value", "" + passableIDArray[i]);
+                        var t = document.createTextNode("" + passableNameArray[i]);
+                        z.appendChild(t);
+                        this.mPassableSelect.appendChild(z);
+                }
+
+		
+		var that = this;
+	
+		//button and emit	
 		this.button.addEventListener('click', function()
 		{
-			var pictureid = that.mSelect.options[that.mSelect.selectedIndex].value;
+			var pictureid = that.mPictureSelect.options[that.mPictureSelect.selectedIndex].value;
 			that.mSocket.emit('build', pictureid);
 		});
 	},
 	removePickPartyElements: function()
 	{
-        	this.mSelect.parentNode.removeChild(this.mSelect);
+        	this.mPictureSelect.parentNode.removeChild(this.mPictureSelect);
         	this.button.parentNode.removeChild(this.button);
 	}
 });
